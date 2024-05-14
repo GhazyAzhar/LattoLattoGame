7156c4682438edf63c1b39b51fa25995a2fc52b1
GhazyAzhar <mazharghazy@gmail.com>
2024-05-14 18:53:13 +0700

    Initial commit again

--- Letto29_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/AmazonExtrasBuilder.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#import <GoogleMobileAds/GoogleMobileAds.h>
-
-#import "GADUAdNetworkExtras.h"
-
-@interface AmazonExtrasBuilder : NSObject<GADUAdNetworkExtras>
-
-@end
